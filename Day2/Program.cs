397c38bd7827ea4f34623c25a90f68bb6797d776
Roger <rogerholmgren@gmail.com>
2016-12-02 11:12:42 +0100

    Day2 Part1

---
+++ Day2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = "ULL\nRRDDD\nLURDL\nUUUUD\n";
+            string input = System.IO.File.ReadAllText(@"../../input.txt");
+            string code = "";
+            int num = 5;
+
+            foreach (var c in input)
+            {
+                switch (c)
+                {
+                    case 'U':
+                        num = num <= 3 ? num : num - 3;
+                        break;
+                    case 'D':
+                        num = num >= 7 ? num : num + 3;
+                        break;
+                    case 'L':
+                        num = (num == 1 || num == 4 || num == 7) ? num : --num;
+                        break;
+                    case 'R':
+                        num = num % 3 == 0 ? num : ++num;
+                        break;
+                    case '\n':
+                        code += $"{num}";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(code);
+        }
+    }
+}
