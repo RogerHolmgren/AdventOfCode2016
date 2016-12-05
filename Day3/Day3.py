f99a30254cc33abec128d20a818ebc60ab0cd599
Roger <rogerholmgren@gmail.com>
2016-12-05 11:10:19 +0100

    Refactored in function def is_triangle

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-def is_triangle(a,b,c):
+def is_triangle(t):
+    a = int(t[0])
+    b = int(t[1])
+    c = int(t[2])
     return (a+b>c) and (a+c>b) and (c+b>a)
 
 # Start of program
@@ -9,7 +12,7 @@ my_input = open('input.txt', 'r')
 #my_input = ["5 10 25","4 4 24"]
 for sides in my_input:
     t = sides.split()
-    if is_triangle(int(t[0]),int(t[1]),int(t[2])) :
+    if is_triangle(t) :
         possible.append(t)
     total.append(t)
 my_input.close()
